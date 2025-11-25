b78c2d35bf3dc54df77379d5b42a3e1a8723d34d
lumoswiz <lumoswiz@protonmail.com>
2025-11-25 23:34:14 +0100

    feat: basic impl for subscribing to new blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use alloy::providers::{Provider, ProviderBuilder, WsConnect};
+use eyre::Result;
+use futures_util::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenvy::dotenv()?;
+
+    let rpc_url = dotenvy::var("ETH_RPC_URL")?;
+    let ws = WsConnect::new(rpc_url);
+    let provider = ProviderBuilder::new().connect_ws(ws).await?;
+
+    let sub = provider.subscribe_blocks().await?;
+    let mut stream = sub.into_stream();
+
+    while let Some(header) = stream.next().await {
+        println!("Latest block number: {}", header.number);
+    }
+
+    Ok(())
 }
