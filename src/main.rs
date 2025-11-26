eeae3c0f27b071c113aec0a6b5525ad09877a9c1
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-11-26 14:35:26 +0100

    feat: add auction params (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+mod auction;
 mod config;
 
-use CCA::CCAInstance;
-use ValidationHook::ValidationHookInstance;
+use crate::{auction::Auction, config::Config};
 use alloy::{
     primitives::address,
     providers::{Provider, ProviderBuilder, WsConnect},
     sol,
 };
-use config::Config;
 use eyre::Result;
 use futures_util::StreamExt;
 
@@ -30,17 +29,19 @@ async fn main() -> Result<()> {
     let config = Config::from_env()?;
 
     let ws = WsConnect::new(&config.rpc_url).with_max_retries(20);
-    let provider = ProviderBuilder::new().connect_ws(ws).await?;
+    let provider = ProviderBuilder::new()
+        .wallet(config.signer.clone())
+        .connect_ws(ws)
+        .await?;
 
-    let _cca = CCAInstance::new(
-        address!("0x608c4e792C65f5527B3f70715deA44d3b302F4Ee"),
-        &provider,
-    );
+    let cca_addr = address!("0x608c4e792C65f5527B3f70715deA44d3b302F4Ee");
+    let hook_addr = address!("0x2DD6e0E331DE9743635590F6c8BC5038374CAc9D");
 
-    let _validation_hook = ValidationHookInstance::new(
-        address!("0x2DD6e0E331DE9743635590F6c8BC5038374CAc9D"),
-        &provider,
-    );
+    let auction = Auction::new(provider.clone(), cca_addr, hook_addr);
+    let params = auction.load_params().await?;
+    let submit_params = auction
+        .prepare_submit_bid(&config.bid_params, &params, config.bid_params.owner)
+        .await?;
 
     let sub = provider.subscribe_blocks().await?;
     let mut stream = sub.into_stream();
