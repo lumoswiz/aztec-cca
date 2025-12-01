e70f2be209a2449354e177e4b6fb94bd234ab9f2
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-12-01 14:32:25 +0100

    refactor: inline sol contracts (#27)

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,56 @@ use crate::{app::AuctionBot, config::Config, logging::init_logging};
 use alloy::{providers::ProviderBuilder, sol};
 use eyre::Result;
 
-sol!(
+sol! {
     #[sol(rpc)]
     #[derive(Debug)]
-    CCA,
-    "abi/cca.json"
-);
+    contract CCA {
+        struct Tick {
+            uint256 next;
+            uint256 currencyDemandQ96;
+        }
 
-sol!(
-    #[sol(rpc)]
-    #[derive(Debug)]
-    ValidationHook,
-    "abi/validation_hook.json"
-);
+        function floorPrice() external view returns (uint256);
+        function tickSpacing() external view returns (uint256);
+        function MAX_BID_PRICE() external view returns (uint256);
+        function endBlock() external view returns (uint64);
+        function ticks(uint256 price) external view returns (Tick memory tick);
+        function submitBid(
+            uint256 maxPrice,
+            uint128 amount,
+            address owner,
+            bytes hookData
+        ) external payable returns (uint256);
+        function submitBid(
+            uint256 maxPrice,
+            uint128 amount,
+            address owner,
+            uint256 prevTickPrice,
+            bytes hookData
+        ) external payable returns (uint256);
+    }
+}
 
-sol!(
+sol! {
     #[sol(rpc)]
     #[derive(Debug)]
-    Soulbound,
-    "abi/soulbound.json"
-);
+    contract ValidationHook {
+        function CONTRIBUTOR_PERIOD_END_BLOCK() external view returns (uint256);
+        function MAX_PURCHASE_LIMIT() external view returns (uint256);
+        function totalPurchased(address sender)
+            external
+            view
+            returns (uint256 totalPurchased);
+    }
+}
+
+sol! {
+    #[sol(rpc)]
+    #[derive(Debug)]
+    contract Soulbound {
+        function hasAnyToken(address _addr) external view returns (bool);
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
